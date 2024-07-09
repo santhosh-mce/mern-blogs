6db0df963d3406d6a7d433544789af9d5f955f5f
santhosh-mce <santhosh.mce1234@gmail.com>
2024-07-09 14:06:41 +0530

    first commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -64,14 +64,14 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="Santhosh blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
+            <Footer.Icon href='https://github.com/Santhosh1507' icon={BsGithub}/>
 
           </div>
         </div>
