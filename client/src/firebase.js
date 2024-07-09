dfafeaf0bb37e255f787bed0935b5836ea89bdb4
santhosh-mce <santhosh.mce1234@gmail.com>
2024-07-09 13:50:58 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT75Dgmv8Pq9SWXIoUp8DI5c0MPUITzXs",
+  authDomain: "blog-page-17777.firebaseapp.com",
+  projectId: "blog-page-17777",
+  storageBucket: "blog-page-17777.appspot.com",
+  messagingSenderId: "763819975825",
+  appId: "1:763819975825:web:af203e6268086979bf0b30"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Snthosh Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Graduated from Meenakshi College with a B.E in Computer Science
+              and Engineering, achieving a total grade point average of 7.4%. My
+              academic journey has equipped me with a solid foundation and a
+              drive to harness technology in addressing real-world challenges.
+            </p>
+
+            <p>
+              Became more interested in IT and decided to try programming.
+              Underwent python course and made a project.
+            </p>
+
+            <p>
+              Worked out in which field I want to build a developer's career and
+              started to learn web Development.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
